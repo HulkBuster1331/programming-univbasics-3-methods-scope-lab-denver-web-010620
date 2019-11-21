24726155b18e5f8bd5039ba0ea546458115c0be9
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-21 20:19:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,9 +14,4 @@ def link
 end
 
 def any_phrase (phrase)
-  describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
-end
+ 
